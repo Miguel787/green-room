b3edb69a4a69c279734334aa51982b92685b8ff8
miguel787 <miguelraulcruz@gmail.com>
2021-04-14 03:04:35 -0400

    added scss to components and routes

--- green-room/Client/src/App.js
+++ green-room/Client/src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./pages/Home.jsx";
-import Header from "./components/Header.jsx";
-import About from "./pages/About.jsx";
-import SignUp from "./pages/SignUp.jsx";
+import Home from "./pages/Home/Home.jsx";
+import Header from "./components/Header/Header.jsx";
+import About from "./pages/About/About.jsx";
+import SignUp from "./pages/SignUp/SignUp.jsx";
 function App() {
   return (
     <div>
