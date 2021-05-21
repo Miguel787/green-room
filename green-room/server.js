623d94dfab4c1b513ea8027b342d0816ed5c4d13
miguel787 <miguelraulcruz@gmail.com>
2021-05-20 23:39:59 -0400

    working on heroku deployment

--- green-room/server.js
+++ green-room/server.js
@@ -1,13 +1,20 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 const venuesRoutes = require("./server/routes/venues");
 
 app.use("/api/venues", venuesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    req.sendFile(path.resolve(__dirname, "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Express is running on port 8080`);
